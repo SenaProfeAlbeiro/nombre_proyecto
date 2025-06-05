57ad671f5fa625a21e17af72fb81bcfa906e61ab
SenaProfeAlbeiro <earamos42@misena.edu.co>
2025-06-05 13:09:22 -0500

    Métodos de persistencia a la base de datos ROLES - CRUD

--- 03_backend/ej01_n_capas_php_sql_html/index.php
+++ 03_backend/ej01_n_capas_php_sql_html/index.php
@@ -1,14 +1,14 @@
 <?php
-    require_once "modelos/DataBase.php";    
-    if (!isset($_REQUEST['c'])) {
-        require_once "controladores/Usuarios.php";
-        $controlador = new Usuarios;
-        $controlador->rolRegistrar();        
-    } else {        
-        $controlador = $_REQUEST['c'];        
-        require_once "controladores/" . $controlador . ".php";        
-        $controlador = new $controlador;        
-        $accion = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'main';
-        call_user_func(array($controlador, $accion));
-    }
+  require_once "modelos/DataBase.php";
+  if (!isset($_REQUEST['c'])) {
+    require_once "controladores/Usuarios.php";
+    $controlador = new Usuarios;
+    $controlador->rolRegistrar();
+  } else {        
+    $controlador = $_REQUEST['c'];
+    require_once "controladores/" . $controlador . ".php";
+    $controlador = new $controlador;        
+    $accion = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'main';
+    call_user_func(array($controlador, $accion));
+  }
 ?>

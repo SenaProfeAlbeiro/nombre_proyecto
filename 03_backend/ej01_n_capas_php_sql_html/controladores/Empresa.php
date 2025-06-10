f651dcfccac5d0a293b2ba9c3b08d01b53f0246a
SenaProfeAlbeiro <earamos42@misena.edu.co>
2025-06-09 19:38:34 -0500

    Implementación de las vistas

--- 03_backend/ej01_n_capas_php_sql_html/controladores/Empresa.php
+++ 03_backend/ej01_n_capas_php_sql_html/controladores/Empresa.php
@@ -3,5 +3,14 @@
     public function main(){      
       require_once "vistas/empresa/index.vista.php";
     }
+    public function about(){
+      require_once "vistas/empresa/about.vista.php";      
+    }
+    public function shop(){
+      require_once "vistas/empresa/shop.vista.php";      
+    }
+    public function contact(){
+      require_once "vistas/empresa/contact.vista.php";      
+    }
   }
 ?>
